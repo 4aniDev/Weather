53a05b9d6ac9e60da895bd0c0abf437848b8bbf6
Dan <105542989+4aniDev@users.noreply.github.com>
2023-03-13 12:44:23 +0500

    Lesson 12 - "TabLayout" (TabRow + Tab + HorizontalPager)

--- app/src/main/java/ru/chani/weather/MainActivity.kt
+++ app/src/main/java/ru/chani/weather/MainActivity.kt
@@ -3,21 +3,38 @@ package ru.chani.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import ru.chani.weather.screens.MainCard
+import ru.chani.weather.screens.TabLayout
 import ru.chani.weather.ui.theme.WeatherTheme
+
+const val API_KEY = "d4f6f14308e54a9880f62118232802"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WeatherTheme {
-
+            WeatherTheme() {
+                Image(
+                    painter = painterResource(id = R.drawable.bg),
+                    contentDescription = "background",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.8f)
+                )
+                Column() {
+                    MainCard()
+                    TabLayout()
+                }
             }
         }
     }
---
+++ app/src/main/java/ru/chani/weather/screens/MainCard.kt
@@ -0,0 +1,157 @@
+package ru.chani.weather.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
+import ru.chani.weather.R
+import ru.chani.weather.ui.theme.BlueLight
+import ru.chani.weather.ui.theme.WhiteLight
+
+@Preview
+@Composable
+fun MainCard() {
+    Column(
+        modifier = Modifier.padding(5.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            backgroundColor = WhiteLight,
+            elevation = 0.dp,
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 8.dp, start = 8.dp),
+                        text = "01.03.2023",
+                        style = TextStyle(fontSize = 15.sp)
+                    )
+                    AsyncImage(
+                        model = "https://turkmenportal.com/themes/turkmenportal/img/logo_tp.png",
+                        contentDescription = "icon",
+                        modifier = Modifier
+                            .size(60.dp)
+                            .padding(end = 8.dp)
+                    )
+
+                }
+                Text(
+                    text = "Ashgabat",
+                    style = TextStyle(fontSize = 24.sp),
+                )
+                Text(
+                    text = "45°C",
+                    style = TextStyle(fontSize = 65.sp)
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp)
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.search),
+                            contentDescription = "search"
+                        )
+                    }
+                    Text(
+                        text = "24°C / 48°C",
+                        style = TextStyle(fontSize = 16.sp)
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.refresh),
+                            contentDescription = "refresh"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("HOURS", "DAYS")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .padding(5.dp)
+            .clip(RoundedCornerShape(5.dp))
+    ) {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { position ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, position)
+                )
+            },
+            backgroundColor = WhiteLight
+        ) {
+            tabList.forEachIndexed { index, string ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    text = { Text(text = string) }
+                )
+            }
+        }
+
+        HorizontalPager(
+            count = tabList.size,
+            state = pagerState,
+            modifier = Modifier.weight(1.0f)
+        ) { index ->
+            when (index) {
+                0 -> {
+                    AsyncImage(
+                        model = "https://tmcars.info/tmcars/images/original/2023/03/13/10/46/1ae136fb-df0f-4c6d-b014-71aaaf8b6d62.jpg",
+                        contentDescription = "car"
+                    )
+                }
+                1 -> {
+                    AsyncImage(
+                        model = "https://tmcars.info/tmcars/images/original/2023/03/13/12/17/43239c72-76f3-4555-8cd8-1d9e6a2484cd.jpg",
+                        contentDescription = "phone",
+                    )
+                }
+            }
+        }
+    }
+}
